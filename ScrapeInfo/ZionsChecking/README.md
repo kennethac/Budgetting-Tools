3c8e0e4c2b4ae25e1cf58e54d941a695684f1dae
Kenneth Christensen <1kennethac@gmail.com>
2020-02-23 16:58:02 -0700

    Move to sub folder.

---
+++ ScrapeInfo/ZionsChecking/README.md
@@ -0,0 +1,11 @@
+# Zions Checking
+
+Because Zions Checking does not offer a way to export transaction data, I wrote this little script to export them by pulling the data out of the table on the web portal.
+
+## Usage
+
+1. Log into the Zions Bank website and navigate to the page with the transactions to download. 
+
+2. Open the Developer Tools.
+
+Copy the code in ZionsChecking.js into the 
--- ScrapeInfo/ZionsChecking/ZionsChecking.js
+++ ScrapeInfo/ZionsChecking/ZionsChecking.js
@@ -14,18 +14,25 @@ async function getCSVForPage(lastPage = false) {
     // Expand all the "more" boxes
     [...document.querySelectorAll("small")].filter(e => e.innerText == 'More').forEach(e => e.click())
     await pause(500);
-    const descriptions = [...document.querySelectorAll(".table_column_2")].map(e => e.innerText);
-    const dates = [...document.querySelectorAll(".table_column_0")].map(e => e.innerText);
-    const amounts = [...document.querySelectorAll(".table_column_4")].map(e => {
+    var secondTable = document.querySelectorAll("#listTableLinkBody")[1]
+    const descriptions = [...secondTable.querySelectorAll(".table_column_2 .changeText")].map(e => e.innerText.trim().replace(/,/g, ' '));
+    const dates = [...secondTable.querySelectorAll(".table_column_0")].map(e => e.innerText.trim());
+    const debits = [...secondTable.querySelectorAll(".table_column_4")].map(e => {
         var match = e.innerText.match(/\d+\.\d\d/);
         if (match == null) return "";
-        return match[0];
+        return match[0].trim();
     });
 
-    if (descriptions.length != dates.length || descriptions.length != amounts.length) {
+    const credits = [...secondTable.querySelectorAll(".table_column_5")].map(e => {
+        var match = e.innerText.match(/\d+\.\d\d/);
+        if (match == null) return "";
+        return match[0].trim();
+    });
+
+    if (descriptions.length != dates.length || descriptions.length != credits.length || descriptions.length != debits.length) {
 
         alert("uneven numbers of rows in each column problem.");
-        console.error(descriptions.length, dates.length, amounts.length);
+        console.error(descriptions.length, dates.length, debits.length, credits.length);
         return;
     }
 
@@ -50,7 +57,16 @@ async function getCSVForPage(lastPage = false) {
     };
 
     for (var i = 0; i < descriptions.length; i++) {
-        appendCsv(dates[i], descriptions[i], amounts[i]);
+        let credit = credits[i];
+        let debit = debits[i];
+        console.log(`"${credit}" "${debit}"`);
+        var amount;
+        if (credit.trim() == "") {
+            amount = `-${debit}`;
+        } else {
+            amount = credit;
+        }
+        appendCsv(dates[i], descriptions[i], amount);
     }
 
     if (lastPage) {
